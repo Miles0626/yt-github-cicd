56a398ef9b0efffeabd8fb572200d18ed1a6a580
caibo <shelby-family@outlook.com>
2026-07-12 20:59:26 +0800

    add a http server

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println(Hello())
 	fmt.Println(Ping())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello World Go K8s Demo")
+	})
+	fmt.Println("服务启动，监听8080端口")
+	http.ListenAndServe(":8080", nil)
 }
 
 func Hello() string {
